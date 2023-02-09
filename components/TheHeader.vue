db52d9a4cae6a9c22768366ea304115003f290bc
Gardo Kirsipuu <gardo1999@gmail.com>
2023-02-09 14:06:25 +0200

    Created header

---
+++ components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="py-4 flex flex-row flex-wrap justify-center items-center bg-teal-400">
+        <div class="basis-1/4 px-8 text-4xl">
+            <Icon class="text-5xl" name="material-symbols:menu-book-outline" />
+            <a>Librafy</a>
+        </div>
+        <div class="basis-1/2 text-center">
+            <input class="w-3/4 text-2xl rounded-lg p-2" placeholder="Otsi raamat"/>
+        </div>
+        <div class="basis-1/4 text-end px-8">
+            <a href="#" class="text-4xl">Logi sisse</a>
+        </div>
+    </div>
+</template>
