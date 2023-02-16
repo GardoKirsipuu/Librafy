7f73625b513278998e7eb1fb90a7489bcd05b1ee
Gardo Kirsipuu <gardo1999@gmail.com>
2023-02-16 15:46:24 +0200

    Created login modal

---
+++ components/LoginModal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto f-full w-full">
+        <div class="relative inset-y-20 mx-auto p-5 sm:w-[36rem] w-11/12 shadow-lg rounded-2xl bg-teal-400">
+            <div class="flex flex-row text-xl justify-between">
+                <h1 class="">Logi sisse</h1>
+                <Icon @click="$emit('closeModal')" class="text-2xl cursor-pointer" name="material-symbols:close" />
+            </div>
+            <div class="mt-4 grid justify-items-center">
+                <input class="rounded-2xl p-3 mb-4 w-full" placeholder="E-post"/>
+                <input class="rounded-2xl p-3 mb-4 w-full" placeholder="Parool"/>
+                <p class="text-center mb-4">Pole kasutajat? Registreeri <a class="underline font-normal cursor-pointer hover:font-semibold" onClick="">siin</a></p>
+                <button class="rounded-2xl p-3 w-1/3 bg-teal-700 text-white">Logi sisse</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-screen py-4 flex flex-row flex-wrap justify-center items-center bg-teal-400">
+    <div class="max-w-screen py-4 flex flex-row flex-wrap justify-center items-center bg-teal-400 shadow-lg">
         <div class="md:order-1 basis-1/2 md:basis-1/4 md:text-3xl text-2xl px-8">
             <NuxtLink class="flex flex-row" to="/">
                 <Icon class="md:text-4xl text-3xl" name="material-symbols:menu-book-outline" />
@@ -8,10 +8,16 @@
         </div>
         <div class="md:order-3 basis-1/2 md:basis-1/4 text-end pr-8 md:text-3xl text-2xl">
             <Icon name="material-symbols:account-circle" />
-            <a href="#" class="align-middle">Logi sisse</a>
+            <a href="#" class="align-middle" @click="showLoginModal = !showLoginModal">Logi sisse</a>
         </div>
         <div class="md:order-2 basis-full md:basis-2/4 text-center mt-4 md:mt-0">
-            <input class="w-3/4 text-2xl rounded-lg py-2" placeholder="Otsi raamat"/>
+            <input class="md:w-3/4 text-2xl rounded-2xl p-2" placeholder="Otsi raamat"/>
         </div>
     </div>
+
+    <LoginModal v-show="showLoginModal" @close-modal="showLoginModal = !showLoginModal"/>
 </template>
+
+<script setup>
+    const showLoginModal = ref(false)
+</script>
